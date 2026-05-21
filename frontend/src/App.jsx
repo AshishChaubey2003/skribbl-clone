19f6b2e769e59d931ecbbae8d938677d712b19c7
Ashish Kumar Chaubey <118584581+AshishChaubey2003@users.noreply.github.com>
2026-05-21 13:28:35 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,54 +1,43 @@
 // App.jsx
 // Controls which screen is shown: Home → Lobby → Game → EndGame
-
 import { useState } from "react";
 import Home from "./screens/Home";
 import Lobby from "./screens/Lobby";
 import Game from "./screens/Game";
 import EndGame from "./screens/EndGame";
 import { connectSocket } from "./socket";
-
 const SCREENS = {
   HOME: "home",
   LOBBY: "lobby",
   GAME: "game",
   END: "end",
 };
-
 export default function App() {
   const [screen, setScreen] = useState(SCREENS.HOME);
   const [myName, setMyName] = useState("");
   const [roomId, setRoomId] = useState("");
   const [isHost, setIsHost] = useState(false);
   const [gameResult, setGameResult] = useState(null);
-
   async function handleCreateRoom(name, settings) {
-    const res = await fetch("http://localhost:8000/create-room", {
+    const res = await fetch("https://skribbl-clone-qxao.onrender.com/create-room", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ hostName: name, settings }),
     });
     const data = await res.json();
-
     setMyName(name);
     setRoomId(data.roomId);
     setIsHost(true);
-
     connectSocket(data.roomId, name);
     setScreen(SCREENS.LOBBY);
   }
-
-  
   async function handleJoinRoom(name, code) {
-   
-    const res = await fetch(`http://localhost:8000/check-room/${code}`);
+    const res = await fetch(`https://skribbl-clone-qxao.onrender.com/check-room/${code}`);
     const data = await res.json();
-
     if (!data.exists) {
       alert("Room not found! Check the room code.");
       return;
     }
-
     setMyName(name);
     setRoomId(code);
     setIsHost(false);
@@ -58,12 +47,10 @@ export default function App() {
   function handleGameStart(msg) {
     setScreen(SCREENS.GAME);
   }
-
   function handleGameOver(msg) {
     setGameResult(msg);
     setScreen(SCREENS.END);
   }
-
   function handlePlayAgain() {
     setScreen(SCREENS.HOME);
     setMyName("");
@@ -71,13 +58,11 @@ export default function App() {
     setIsHost(false);
     setGameResult(null);
   }
-
   return (
     <div>
       {screen === SCREENS.HOME && (
         <Home onCreateRoom={handleCreateRoom} onJoinRoom={handleJoinRoom} />
       )}
-
       {screen === SCREENS.LOBBY && (
         <Lobby
           roomId={roomId}
@@ -86,11 +71,9 @@ export default function App() {
           onGameStart={handleGameStart}
         />
       )}
-
       {screen === SCREENS.GAME && (
         <Game myName={myName} onGameOver={handleGameOver} />
       )}
-
       {screen === SCREENS.END && gameResult && (
         <EndGame
           winner={gameResult.winner}
