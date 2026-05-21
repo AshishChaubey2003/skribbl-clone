863a6f030faf8e6fff972cec7b1e5cc3a68315a2
Ashish Kumar Chaubey <118584581+AshishChaubey2003@users.noreply.github.com>
2026-05-21 13:27:10 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,12 +1,9 @@
 // socket.js
 // WebSocket connection to FastAPI backend
-
 let socket = null;
 let listeners = {};
-
 export function connectSocket(roomId, playerName) {
-  socket = new WebSocket(`ws://localhost:8000/ws/${roomId}/${playerName}`);
-
+  socket = new WebSocket(`wss://skribbl-clone-qxao.onrender.com/ws/${roomId}/${playerName}`);
   socket.onmessage = (event) => {
     const message = JSON.parse(event.data);
     const type = message.type;
@@ -14,28 +11,23 @@ export function connectSocket(roomId, playerName) {
       listeners[type].forEach((cb) => cb(message));
     }
   };
-
   socket.onopen = () => console.log("Connected to server");
   socket.onclose = () => console.log("Disconnected from server");
 }
-
 export function sendMessage(message) {
   if (socket && socket.readyState === WebSocket.OPEN) {
     socket.send(JSON.stringify(message));
   }
 }
-
 export function onMessage(type, callback) {
   if (!listeners[type]) listeners[type] = [];
   listeners[type].push(callback);
 }
-
 export function offMessage(type, callback) {
   if (listeners[type]) {
     listeners[type] = listeners[type].filter((cb) => cb !== callback);
   }
 }
-
 export function closeSocket() {
   if (socket) socket.close();
 }
